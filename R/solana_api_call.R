cb44520f644129b56e1679335a2354a86632c31d
Trevor French <trevorf96@gmail.com>
2023-07-20 20:28:03 -0400

    get_signature_for_address method

--- R/solana_api_call.R
+++ R/solana_api_call.R
@@ -1,6 +1,7 @@
 #' solana_api_call
 #'
 #' @param url the RPC url for your API call
+#' @param request_body the request body for your API call
 #'
 #' @return returns data from your solana API call
 #' @export
@@ -10,8 +11,34 @@
 #' url <- "http://localhost:8899"
 #' data <- solana_api_call(url)}
 
-solana_api_call <- function(url) {
+solana_api_call <- function(url, request_body) {
   headers <- c("Content-Type" = "application/json")
+
+  response <- httr::POST(url, httr::add_headers(.headers = headers), body = request_body)
+
+  if (httr::status_code(response) == 200) {
+    return(httr::content(response))
+  } else {
+    print(httr::content(response, as = "text"))
+    stop("Request failed with status code: ", httr::status_code(response))
+  }
+}
+
+#' get_signature_for_address
+#'
+#' @param url the RPC url for your API call
+#'
+#' @return Returns signatures for confirmed transactions that include the given
+#' address in their accountKeys list. Returns signatures backwards in time from
+#' the provided signature or most recent confirmed block
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' url <- "http://localhost:8899"
+#' data <- get_signature_for_address(url)}
+
+get_signature_for_address <- function(url) {
 
   request_body <- '{
     "jsonrpc": "2.0",
@@ -20,12 +47,5 @@ solana_api_call <- function(url) {
     "params": ["7H3bquJXx8nhnYUDFcJixrkX8GkpgL8Sv4ZGP34M9gnx", {"limit": 1000}]
   }'
 
-  response <- httr::POST(url, httr::add_headers(.headers = headers), body = request_body)
-
-  if (httr::status_code(response) == 200) {
-    return(httr::content(response, as = "text"))
-  } else {
-    print(httr::content(response, as = "text"))
-    stop("Request failed with status code: ", httr::status_code(response))
-  }
+  solana_api_call(url, request_body)
 }
